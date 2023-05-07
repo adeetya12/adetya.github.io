bfd597945a394e372d6246fbc4cccfc370e0fa46
adeetya12 <adityaalistanto2@gmail.com>
2023-05-08 01:20:07 +0700

    asu

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log('search')
 
 
 window.onload = () => {
-	document.querySelector('.about p').classList.add('active'); console.log('ok')
+	document.querySelector('.about p').classList.add('active'); 
 }
 
 
@@ -17,7 +17,8 @@ document.addEventListener('scroll', function() {
     		document.querySelector('.jumbotron h1').style.transform = `translate(0, ${scrollTop/3}px)`;
     		document.querySelector('.jumbotron h3').style.transform = `translate(0, ${scrollTop/3}px)`;
     		document.querySelector('.jumbotron p').style.transform = `translate(0, ${scrollTop/2}px)`;
-    		if(scrollTop > 400){
+    		
+				if(scrollTop > 400){
 
 				const thumbnails = document.querySelectorAll('.thumbnail');
 
@@ -25,7 +26,7 @@ document.addEventListener('scroll', function() {
 				  thumbnail.classList.add('active');
 				});
 				//document.querySelector('.Project .thumbnail').classList.add('active')
-console.log('ok')
+
 			}
 			if(scrollTop < 400){
 				const thumbnailsR = document.querySelectorAll('.thumbnail.active');
